7aaf911e5cd82ddb46dd2dc55185a3005bd0a35d
edurevsky <edurevsky@gmail.com>
2023-05-21 15:51:14 -0300

    feat: add cow language enum

---
+++ src/main/java/com/github/edustocchero/cowgpt/enums/CowLanguage.java
@@ -0,0 +1,27 @@
+package com.github.edustocchero.cowgpt.enums;
+
+import com.github.edustocchero.cowgpt.cows.Cow;
+import com.github.edustocchero.cowgpt.cows.EnglishCow;
+import com.github.edustocchero.cowgpt.cows.JapaneseCow;
+import com.github.edustocchero.cowgpt.cows.PortugueseCow;
+
+public enum CowLanguage implements CowStrategy {
+    EN {
+        @Override
+        public Cow getCow() {
+            return EnglishCow.getInstance();
+        }
+    },
+    PT {
+        @Override
+        public Cow getCow() {
+            return PortugueseCow.getInstance();
+        }
+    },
+    JP {
+        @Override
+        public Cow getCow() {
+            return JapaneseCow.INSTANCE;
+        }
+    }
+}
---
+++ src/main/java/com/github/edustocchero/cowgpt/enums/CowStrategy.java
@@ -0,0 +1,7 @@
+package com.github.edustocchero.cowgpt.enums;
+
+import com.github.edustocchero.cowgpt.cows.Cow;
+
+public interface CowStrategy {
+    Cow getCow();
+}
