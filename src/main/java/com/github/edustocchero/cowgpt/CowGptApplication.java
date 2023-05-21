866717ce844a4f892066ed2333353a27125a2a3e
edurevsky <edurevsky@gmail.com>
2023-05-21 13:47:12 -0300

    chore: init

---
+++ src/main/java/com/github/edustocchero/cowgpt/CowGptApplication.java
@@ -0,0 +1,13 @@
+package com.github.edustocchero.cowgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CowGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/edustocchero/cowgpt/CowGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.edustocchero.cowgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
